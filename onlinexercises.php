70f5b1319dc8cc7915c85a93085f241c2bc4a19b
Jenniline <jennilineebai@gmail.com>
2020-03-06 22:59:17 +0100

    commiting exercises done online

---
+++ onlinexercises.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+$num=8;
+echo "Value is now $num . <br>";
+
+$num+=2;
+echo "Add 2 value is now $num . <br>";
+
+echo "Subtract 4";
+$num  -= 4;
+echo "Value is now $num . <br>";
+
+echo "multiply by 5";
+$num *= 5;
+echo "Value is now $num . <br>";
+
+echo "Divide by 3";
+$num /=  3;
+echo "Value is now $num . <br>";
+
+echo "increment value by one";
+ $num++;
+ echo "Value is now $num . <br>";
+
+echo "decrement value by one";
+$num--;
+echo "Value is now $num . <br>";
+
+
+
+?>
